b5b746ff1a5dea9bac4946902bb809f37e717ba7
minz <minzoen.nn@gmail.com>
2021-01-09 00:57:27 +0700

    ui table

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -11,185 +11,225 @@
       </div>
     </div>
 
-    <!--<div class="row justify-content-between mx-0 mb-3">
-      <div class="col-md-4 px-0">
-        <div class="w-100 bg-white rounded shadow">
-          <div class="row mx-0">
-            <div class="col-12 px-0">
-              <div class="w-100">
-              <img src="../assets/images/lg.png" class="img-fluid">
+    <div class="w-100 bg-white shadow p-3">
+    <div class="ant-table ant-table-scroll-position-left ant-table-default ant-table-bordered">
+    <div class="ant-table-content">
+    <div class="ant-table-body">
+      <table class="">
+      <thead class="ant-table-thead">
+        <tr>
+        <th rowspan="2">ยี่ห้อ</th>
+        <th colspan="3">คลัง</th>
+        <th rowspan="2">แก้ไข</th>
+        </tr>
+        <tr>
+          <th>9</th>
+          <th>12</th>
+          <th>18</th>
+        </tr>
+      </thead>
+      <tbody class="ant-table-tbody">
+        <tr class="ant-table-row ant-table-row-level-0" v-for="(product, key) in products" :key="key">
+          <td class="ant-table-row-cell-break-word">
+            <div v-if="model_update_product.key !== key">
+              <div class="text-center" :class="{'position-relative': product.amount == 0}">
+                <img :src="require(`@/assets/images/${product.img}`)" height="40">
+                <div class="position-absolute" v-if="product.amount == 0">
+                  <a-tag color="red">
+                    หมด
+                  </a-tag>
+                </div>
               </div>
             </div>
-            <div class="col-12 mb-2">
-              <div class="row mx-0">
-                <div class="col px-0">
-                  <span class="text-black-45 font-size-12">BTU</span>
-                </div>
-                <div class="col px-0 text-center">
-                  <span class="text-black-45 font-size-12">จำนวน</span>
-                </div>
-                <div class="col px-0"></div>              
-              </div>
+            <a-input v-else v-model="model_update_product.name" placeholder="Name" size="small"></a-input>
+          </td>
+          <td class="ant-table-row-cell-break-word font-weight-bold" :class="{'text-danger':product.btu9 == 0}">
+            <div v-if="model_update_product.key !== key">
+            {{product.btu9}}
             </div>
-            <div class="col-12 mb-3">
-              <div class="row mx-0">
-                <div class="col px-0">
-                  <span class="text-black-85 font-size-16">9,000</span>
-                </div>
-                <div class="col px-0 text-center">
-                  <span class="text-black-85 font-size-16 font-weight-bold">2</span>
-                </div>
-                <div class="col px-0 text-right">
-                  <a-button :size="'small'"><a-icon type="edit"/></a-button>
-                </div>              
-              </div>
-            </div>
-            <div class="col-12 mb-3">
-              <div class="row mx-0">
-                <div class="col px-0">
-                  <span class="text-black-85 font-size-16">12,000</span>
-                  <a-tag class="font-size-14">9,000</a-tag>
-                </div>
-                <div class="col px-0 text-center">
-                  <span class="text-danger font-size-16 font-weight-bold">0</span>
-                </div>
-                <div class="col px-0 text-right">
-                  <a-button :size="'small'"><a-icon type="edit"/></a-button>
-                </div>              
-              </div>
-            </div>
-            <div class="col-12 mb-3">
-              <div class="row mx-0">
-                <div class="col px-0">
-                  <span class="text-black-85 font-size-16">18,000</span>
-                </div>
-                <div class="col px-0 text-center">
-                  <span class="text-danger font-size-16 font-weight-bold">0</span>
-                </div>
-                <div class="col px-0 text-right">
-                  <a-button :size="'small'"><a-icon type="edit"/></a-button>
-                </div>              
-              </div>
-            </div>            
-          </div>
-        </div>
-      </div>
-    </div>-->
-    <div class="w-100 rounded bg-white shadow p-3">
-      <a-table bordered :columns="columns" :data-source="data">
-        <!--body-->
-        <div class="text-center" slot="img" slot-scope="logo" :class="{'position-relative': data.amount == 0}">
-          <img :src="require(`@/assets/images/${logo}`)" height="40">
-          <div class="position-absolute" v-if="data.amount == 0">
-            <a-tag color="red">
-              หมด
-            </a-tag>
-          </div>
-        </div>
-        <div class="text-center" slot="btu9" slot-scope="btu9_text">
-          <span class="font-weight-bold" :class="{'text-danger':btu9_text == 0}">{{btu9_text > 0? btu9_text:0}}</span>
-        </div>
-        <div class="text-center" slot="btu12" slot-scope="btu12_text">
-          <span class="font-weight-bold" :class="{'text-danger':btu12_text == 0}">{{btu12_text > 0? btu12_text:0}}</span>
-        </div>
-        <div class="text-center" slot="btu18" slot-scope="btu18_text">
-          <span class="font-weight-bold" :class="{'text-danger':btu18_text == 0}">{{btu18_text > 0? btu18_text:0}}</span>
-        </div>
-        <div class="text-center" slot="amount" slot-scope="get_amount">
-          <span class="font-weight-bold" :class="{'text-danger':get_amount == 0}">{{get_amount > 0? get_amount:0}}</span>
-        </div>
-        <div class="text-center" slot="active">
-            <a-button :size="'small'"><a-icon type="edit"/></a-button>
-        </div>
-      </a-table>
+            <a-input v-else v-model="model_update_product.btu9" size="small"></a-input>
+          </td>
+          <td class="ant-table-row-cell-break-word font-weight-bold" :class="{'text-danger':product.btu12 == 0}">{{product.btu12}}</td>
+          <td class="ant-table-row-cell-break-word font-weight-bold" :class="{'text-danger':product.btu18 == 0}">{{product.btu18}}</td>
+          <td class="ant-table-row-cell-break-word">
+            <a-button :size="'small'" @click="setUpdateContact(key, product)">
+              <a-icon type="edit"/>
+            </a-button>
+            <a-button type="primary" size="small" @click="updateContact(model_update_product)">บันทึก</a-button>
+            <a-button type="default" size="small" class="ml-2" @click="deleteContact(key)">ลบ</a-button>
+          </td>
+        </tr>
+      </tbody>
+      </table>
+    </div>
+    </div>
+    </div>
     </div>
   </div>
 </template>
 
 <script>
-const columns = [
-  {
-    title: 'ยี่ห้อ',
-    dataIndex: 'img',
-    key: 'img',
-    scopedSlots: { customRender: 'img' },
-  },
-  {
-    title: 'คลัง',
-    children: [
-      {
-        title: '9000 BTU',
-        dataIndex: 'btu9',
-        key: 'btu9',
-        scopedSlots: { customRender: 'btu9' },
-      },
-      {
-        title: '12,000 BTU',
-        dataIndex: 'btu12',
-        key: 'btu12',
-        scopedSlots: { customRender: 'btu12' },
-      },
-      {
-        title: '18,000 BTU',
-        dataIndex: 'btu18',
-        key: 'btu18',
-        scopedSlots: { customRender: 'btu18' },
-      }
-    ]
-  },
-  {
-    title: 'จำนวนทั้งหมด',
-    dataIndex: 'amount',
-    key: 'amount',
-    scopedSlots: { customRender: 'amount' },
-  }
-  ,
-  {
-    title: 'Active',
-    key: 'active',
-    scopedSlots: { customRender: 'active' },
-  }
-];
 
-const data = [
-  {
-    key: '1',
-    name: 'samsung',
-    img: 'samsung.png',
-    btu9: 3,
-    btu12: 0,
-    btu18: 0,
-    amount: 3
-  },
-  {
-    key: '2',
-    name: 'daikin',
-    img: 'daikin.png',
-    btu9: 0,
-    btu12: 1,
-    btu18: 0,
-    amount: 1
-  },
-  {
-    key: '3',
-    name: 'haier',
-    img: 'haier.png',
-    btu9: 0,
-    btu12: 0,
-    btu18: 0,
-    amount: 0
-  }
-];
+import * as firebase from 'firebase'
+var config = {
+    apiKey: "AIzaSyBvyXtXZgtJMomfq0jEJBbdgBWYbyL2tjc",
+    authDomain: "pc-air-v1.firebaseapp.com",
+    databaseURL: "https://pc-air-v1-default-rtdb.firebaseio.com",
+    projectId: "pc-air-v1",
+    storageBucket: "pc-air-v1.appspot.com",
+    messagingSenderId: "1024121686169",
+}
+firebase.initializeApp(config)
+
+var database = firebase.database()
+var contactRef = database.ref('/products')
+
+// const data = [
+//   {
+//     key: '1',
+//     name: 'samsung',
+//     img: 'samsung.png',
+//     btu9: 3,
+//     btu12: 0,
+//     btu18: 0,
+//     amount: 3
+//   },
+//   {
+//     key: '2',
+//     name: 'daikin',
+//     img: 'daikin.png',
+//     btu9: 0,
+//     btu12: 1,
+//     btu18: 0,
+//     amount: 1
+//   },
+//   {
+//     key: '3',
+//     name: 'haier',
+//     img: 'haier.png',
+//     btu9: 0,
+//     btu12: 0,
+//     btu18: 0,
+//     amount: 0
+//   }
+// ];
 
 export default {
   name: 'Overview',
-  
   data () {
     return {
-      msg: 'ภาพรวม',
-      data,
-      columns
+      msg: 'Test | Realtime Database',
+      products:{},
+      model_product: {
+        name: '',
+        img: '',
+        btu9: 0,
+        btu12: 0,
+        btu18: 0,
+        amount: 0,
+      },
+      model_update_product:{
+        key: '',
+        name: '',
+        img: '',
+        btu9: '',
+        btu12: '',
+        btu18: '',
+        amount: '',
+      },
+      columns : [
+      {
+        title: 'ยี่ห้อ',
+        dataIndex: 'img',
+        key: 'img',
+        scopedSlots: { customRender: 'img' },
+      },
+      {
+        title: 'คลัง',
+        children: [
+          {
+            title: '9000 BTU',
+            dataIndex: 'btu9',
+            key: 'btu9',
+            scopedSlots: { customRender: 'btu9' },
+          },
+          {
+            title: '12,000 BTU',
+            dataIndex: 'btu12',
+            key: 'btu12',
+            scopedSlots: { customRender: 'btu12' },
+          },
+          {
+            title: '18,000 BTU',
+            dataIndex: 'btu18',
+            key: 'btu18',
+            scopedSlots: { customRender: 'btu18' },
+          }
+        ]
+      },
+      {
+        title: 'จำนวนทั้งหมด',
+        dataIndex: 'amount',
+        key: 'amount',
+        scopedSlots: { customRender: 'amount' },
+      }
+      ,
+      {
+        title: 'Active',
+        key: 'active',
+        scopedSlots: { customRender: 'active' },
+      }
+    ]
     }
+  },
+  methods: {
+    insertToContact (model_product) {
+      contactRef.push(model_product)
+      this.model_product.name = ''
+      this.model_product.img = ''
+      this.model_product.btu9 = 0
+      this.model_product.btu12 = 0
+      this.model_product.btu18 = 0
+      this.model_product.amount = 0
+    }
+    ,
+    setUpdateContact (key, product) {
+      this.model_update_product.key = key
+      this.model_update_product.name = product.name
+      this.model_update_product.img = product.img
+      this.model_update_product.btu9 = product.btu9
+      this.model_update_product.btu12 = product.btu12
+      this.model_update_product.btu18 = product.btu18
+      this.model_update_product.amount = product.amount
+    }
+    ,
+    updateContact(model_update_product) {
+      
+      // console.log(model_update_product)
+      let data = {
+        key:model_update_product.key,
+        name:model_update_product.name,
+        img:model_update_product.img,
+        btu9:model_update_product.btu9,
+        btu12:model_update_product.btu12,
+        btu18:model_update_product.btu18,
+        amount:model_update_product.amount
+      }
+      contactRef.child(this.model_update_product.key).update(data)
+      this.model_update_product.key = ''
+      this.model_update_product.name = ''
+      this.model_update_product.img = ''
+    }
+    ,
+    deleteContact (key) {
+      contactRef.child(key).remove()
+    }
+  },
+  mounted () {
+    contactRef.on('value', (snapshot) => {
+      this.products = snapshot.val()
+      console.log('products ---- ',this.products)      
+    })
   }
 }
 </script>
